14e8dd0bfdb4053d7a216b2af3c901ab7e177315
Max Walley <max.walley1@btinternet.com>
2020-10-31 13:51:59 +0000

    Game currently involves a car that can move forwards and back and responds to keys. An obstacle is also present

---
+++ Unit1-Player-Control/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float speed = Input.GetAxis("Vertical");
+
+        if (speed != 0)
+        {
+            transform.Translate(0, 0, speed);
+        }
+    }
+}
